87acb71d8bd92549be5efa0168cf24ff8989e28f
krish17 <bkmr17@yahoo.com>
2016-05-28 20:03:27 -0400

    Cleaned DSL client

--- src/mics/dsl/client/Client.scala
+++ src/mics/dsl/client/Client.scala
@@ -8,25 +8,33 @@ import mics.dsl.service.Output._
 
 object Client extends App {
 
-  val myService = service("MyTestService") defines "My testing service" at "com.sample.dsl" having modules (
+  val myService = service("Library") defines "Library service application" at "org.library.dsl" having modules (
 
-    module("FirstTestModule") defines "First test module" having actions (
+    module("User") defines "User management" having actions (
 
-        action("doFirstThing") to "Do the first thing" takes (
-            input("inputOne") as "Integer" carries "person's age"
-          ) and returns ("Some output" as "String"),
+        action("register") to "Register a new user" takes (
+            input("userName") as "UserName" carries "Member's user name",
+            input("password") as "Password" carries "user password",
+            input("address") as "Address" carries "user address"
+          ) and returns ("User account" as "User"),
 
-        action("doSecondThing") to "Do the second thing" takes (
-            input("inputOne") as "Double" carries "person's salary",
-            input("inputTwo") as "Double" carries "tax rate"
-          ) and returns ("Some output" as "Double")
+        action("modify") to "Modify user account details" takes (
+            input("userName") as "UserName" carries "Member's user name",
+            input("address") as "Address" carries "Member's new address"
+          ) and returns ("Updated user" as "User")
       ),
 
-    module("SecondTestModule") defines "First test module" having actions (
-        action("doFirstThing") to "Do the first thing",
-        action("doSecondThing") to "Do the second thing"
+    module("Catalog") defines "Books and Magazine catalog management" having actions (
+
+        action("addItem") to "Add a new catalog item" takes (
+            input("item") as "CatalogItem" carries "Catalog item"
+          ) and returns ("Catalog item id" as "CatalogItemDetails"),
+
+        action("removeItem") to "Remove a catalog item" takes (
+            input("itemId") as "CatalogItemId" carries "Catalog item id"
+          ) and returns ("Removed catalog item id" as "CatalogItemId")
       )
   )
 
-  myService.generate("C:/sldkfj/lsdfj")
+  myService.generate("C:/play/activator/projects/library")
 }
